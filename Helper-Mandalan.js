261336af33828a9acfdcc99e1c54f35479f01391
MnD <95845768+omniset@users.noreply.github.com>
2023-03-24 16:36:39 +0700

    Add files via upload

---
+++ Helper-Mandalan.js
@@ -0,0 +1,6 @@
+const MathHelper = require('./mathHelper');
+
+console.log(MathHelper.add(2, 3)); // Output: 5
+console.log(MathHelper.subtract(5, 3)); // Output: 2
+console.log(MathHelper.multiply(2, 4)); // Output: 8
+console.log(MathHelper.divide(10, 2)); // Output: 5
